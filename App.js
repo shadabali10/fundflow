9f264f20d031beb5c1f3f4788050c6c427f7e026
shadab2110058 <shadab2110058@akgec.ac.in>
2023-02-09 17:08:24 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Investor from './Components/Investor'
+
+const App = () => {
+  return (
+    <div>
+        <Investor/>
+    </div>
+  )
+}
+
+export default App
---
+++ Components/Investor.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Investor.css"
+
+const Investor = () => {
+  return (
+    <>
+      <div className='dashboard'></div>
+    </>
+  )
+}
+
+export default Investor
