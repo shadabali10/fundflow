4dbc63e55ab7233a054a595046fea5fab0d04985
Nilay Rudra <109015625+N03Nilay@users.noreply.github.com>
2023-02-09 22:14:01 +0530

    Update Biddingpage.jsx

--- Biddingpage.jsx
+++ Biddingpage.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import {useFormik} from "formik";
 
+
+
 const initialValues = {
     price: "",
     percent: ""
